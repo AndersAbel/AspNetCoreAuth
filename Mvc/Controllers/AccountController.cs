630acaf37065c19acd42d163619b98a7fb33185c
Anders Abel <anders.abel@sustainsys.com>
2024-06-05 23:22:44 +0200

    Complete solution

--- Mvc/Controllers/AccountController.cs
+++ Mvc/Controllers/AccountController.cs
@@ -1,10 +1,11 @@
 ﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
 using Mvc.Models;
 using System.Security.Claims;
 
 namespace Mvc.Controllers;
-public class AccountController : Controller
+public class AccountController(IAuthenticationSchemeProvider schemeProvider) : Controller
 {
     [HttpGet]
     public IActionResult Login()
@@ -26,7 +27,7 @@ public class AccountController : Controller
                         new("email", model.UserName + "@example.com")
                     ];
 
-                if(model.UserName == "Admin")
+                if (model.UserName == "Admin")
                 {
                     claims.Add(new("role", "admin"));
                 }
@@ -44,8 +45,75 @@ public class AccountController : Controller
     }
 
     [HttpPost]
+    public async Task<IActionResult> SignOut(string scheme)
+    {
+        if (scheme == CookieAuthenticationDefaults.AuthenticationScheme)
+        {
+            await HttpContext.SignOutAsync(scheme);
+            return Redirect("/");
+        }
+
+        AuthenticationProperties props = new()
+        {
+            RedirectUri = "/"
+        };
+
+        return SignOut(props, scheme);
+    }
+
+    public IActionResult Challenge(string scheme, string returnUrl)
+    {
+        returnUrl ??= "/";
+
+        if (!Url.IsLocalUrl(returnUrl))
+        {
+            throw new Exception("Hackerzzzz");
+        }
+
+        AuthenticationProperties props = new()
+        {
+            RedirectUri = returnUrl,
+        };
+
+        return Challenge(props, scheme);
+    }
+
+    public IActionResult Forbid(string scheme)
+    {
+        return base.Forbid(scheme);
+    }
+
+    public IActionResult AccessDenied(string returnUrl)
+    {
+        return View(model: returnUrl);
+    }
+
+    public async Task<IActionResult> Authenticate(string scheme)
+    {
+        var authResult = await HttpContext.AuthenticateAsync(scheme);
+
+        return View(authResult);
+    }
+
     public async Task<IActionResult> Logout()
     {
+        var authResult = await HttpContext.AuthenticateAsync();
+
+        if (authResult.Succeeded)
+        {
+            if(authResult.Properties.Items.TryGetValue(".AuthScheme", out var schemeName))
+            {
+                AuthenticationProperties props = new()
+                {
+                    RedirectUri = "/"
+                };
+
+                return SignOut(props,
+                    CookieAuthenticationDefaults.AuthenticationScheme,
+                    schemeName!);
+            }
+        }
+
         await HttpContext.SignOutAsync();
         return Redirect("/");
     }
--- Mvc/Program.cs
+++ Mvc/Program.cs
@@ -1,11 +1,34 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews()
     .AddRazorRuntimeCompilation();
 
-builder.Services.AddAuthentication()
-    .AddCookie();
+builder.Services.AddAuthentication(opt =>
+{
+    opt.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    opt.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+})
+    .AddCookie()
+    .AddOpenIdConnect(opt =>
+    {
+        opt.Authority = "https://demo.duendesoftware.com";
+        opt.ClientId = "interactive.confidential";
+        opt.ClientSecret = "secret";
+        opt.ResponseType = "code";
+
+        opt.SaveTokens = true;
+
+        opt.MapInboundClaims = false;
+        opt.GetClaimsFromUserInfoEndpoint = true;
+
+        opt.TokenValidationParameters.NameClaimType = "name";
+        opt.TokenValidationParameters.RoleClaimType = "role";
+
+    });
 
 var app = builder.Build();
 
@@ -26,6 +49,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{scheme=" + CookieAuthenticationDefaults.AuthenticationScheme + "}");
 
 app.Run();
